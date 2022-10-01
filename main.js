66e01501888265f2ef4a6c34e455d2d9a9976cc2
Pasztor Adam <adam.psztr@icloud.com>
2022-10-01 11:34:21 +0200

    set fetch options

---
+++ main.js
@@ -0,0 +1,20 @@
+function getServerData(url){
+	let fetchOptions = {
+		method: "GET",
+		mode: "cors",
+		cache: "no-cache"
+	};
+
+	return fetch(url, fetchOptions).then(
+		response => response.json(),
+		err => console.error(err)
+	)
+}
+
+function startGetPrice() {
+	getServerData("https://adam-psztr.github.io/bestPrice.src/products.json").then(
+		data => document.querySelector("p").innerText =  data
+	);
+}
+
+startGetPrice();
